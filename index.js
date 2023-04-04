349f5900eac47398e10a204b870978f873e9105f
tata-20 <129096182+tata-20@users.noreply.github.com>
2023-04-05 02:42:36 +0700

    update5

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var bil1= parseInt(document.getElementById("a").value);
 var bil2= parseInt(document.getElementById("t").value);
 var hasil=0.5*bil1*bil2;
 document.getElementById("hasil").value = hasil;
-
-//console.log("L=1/2*", bil1,"*", bil2);
-//console.log("L=", hasil);
+document.getElementById("bil1").value = bil1;
+document.getElementById("bil2").value = bil2;
 }
