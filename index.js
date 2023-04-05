b73f3f6a9e7d36c733f4fa501ac0775f3e55eba8
tata-20 <129096182+tata-20@users.noreply.github.com>
2023-04-05 08:07:49 +0700

    update7

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ document.getElementById("hasil").value = hasil;
 document.getElementById("bil1").value = bil1;
 document.getElementById("bil2").value = bil2;
 }
+
+function keliling(){
+    var s1= parseInt(document.getElementById("s1").value);
+    var s2= parseInt(document.getElementById("s2").value);
+    var s3= parseInt(document.getElementById("s3").value);
+    var jumlah= s1+s2+s3;
+    document.getElementById("jumlah").value = jumlah;
+    document.getElementById("s1").value = s1;
+    document.getElementById("s2").value = s2;
+    document.getElementById("s3").value = s3;
+}
