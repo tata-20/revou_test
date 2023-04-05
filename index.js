6a39d88c3e6b7404b0fec041913e4b060db428f9
tata-20 <129096182+tata-20@users.noreply.github.com>
2023-04-05 09:24:49 +0700

    update9

--- index.js
+++ index.js
@@ -18,3 +18,21 @@ function keliling(){
     document.getElementById("s2").value = s2;
     document.getElementById("s3").value = s3;
 }
+
+function hitung2(){
+    var bil1= parseInt(document.getElementById("a").value);
+    var bil2= parseInt(document.getElementById("t").value);
+    var hasil=bil1*bil2;
+    document.getElementById("hasil").value = hasil;
+    document.getElementById("bil1").value = bil1;
+    document.getElementById("bil2").value = bil2;
+    }
+
+function keliling2(){
+    var angka1= parseInt(document.getElementById("1").value);
+    var angka2= parseInt(document.getElementById("2").value);
+    var jumlah=2*angka1*angka2;
+    document.getElementById("jumlah").value = jumlah;
+    document.getElementById("angka1").value = angka1;
+    document.getElementById("angka2").value = angka2;
+        }
